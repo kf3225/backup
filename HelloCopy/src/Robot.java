4b250b41ccbb9251fc5f36be4b6268dace0e9fbf
kf3225 <clrflg.k.f@ezweb.ne.jp>
2017-12-19 16:33:31 +0900

    12/19

---
+++ HelloCopy/src/Robot.java
@@ -0,0 +1,17 @@
+
+public class Robot {
+	public String robotname=null;
+	
+	public void talk(){
+		System.out.println(this.robotname+"が話す");
+	}
+	
+	public void walk(){
+		System.out.println(this.robotname+"が歩く");
+	}
+	
+	public void run(){
+		System.out.println(this.robotname+"が走る");
+	}
+
+}
